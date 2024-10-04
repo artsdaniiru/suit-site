84cef0cfeeadda2c5dd5febf868637e4d6fde591
Daniiru <k237034@kccollege.ac.jp>
2024-10-05 02:10:46 +0900

    script fix

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -4,12 +4,11 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
     const url = new URL(request.url)
-    console.log(url);
 
     // Если запрос к корню сайта (example.com), перенаправляем на папку пользователя
     if (url.pathname === '/') {
-        return fetch(`https://daniiru.com/~k23b`)
+        return fetch(`https://${url.host}/~k23b`)
     } else {
-        return fetch(`https://daniiru.com/~k23b/` + url.pathname)
+        return fetch(`https://${url.host}/~k23b/${url.pathname}`)
     }
 }
