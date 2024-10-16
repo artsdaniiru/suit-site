2a14e78d37365288810086467d350dc28e0b9180
Daniiru <k237034@kccollege.ac.jp>
2024-10-16 20:54:03 +0900

    cloudflare fix

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -5,10 +5,18 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
     const url = new URL(request.url)
 
+    // Клонируем исходный запрос, чтобы сохранить все его свойства (метод, заголовки, тело и т.д.)
+    const init = {
+        method: request.method,
+        headers: request.headers,
+        body: request.method !== 'GET' && request.method !== 'HEAD' ? await request.clone().arrayBuffer() : null, // Клонируем тело запроса, если оно есть
+        redirect: 'manual' // Перенаправления обрабатываем вручную
+    };
+
     // Если запрос к корню сайта (example.com), перенаправляем на папку пользователя
     if (url.pathname === '/') {
-        return fetch(`https://${url.host}/~k23b`)
+        return fetch(`https://${url.host}/~k23b${url.search}`, init)
     } else {
-        return fetch(`https://${url.host}/~k23b/${url.pathname}`)
+        return fetch(`https://${url.host}/~k23b${url.pathname}${url.search}`, init)
     }
 }
