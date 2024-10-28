9c5d3c109b5519495f2a2fbc1ecc0e55371a1957
Daniiru <k237034@kccollege.ac.jp>
2024-10-28 11:12:49 +0900

    disable hot reload in public

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,17 @@
 const {
   defineConfig
-} = require('@vue/cli-service')
+} = require('@vue/cli-service');
+
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+      watch: false, // Отключаем наблюдение за всеми файлами в папке public
+    },
+    watchFiles: ['src/**/*'], // Следим только за файлами в src
+  },
 })
 
 // module.exports = {
