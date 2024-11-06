cb2e3efc4d5af10f83d1bf47e524d7bfb6bc4f70
Daniiru <k237034@kccollege.ac.jp>
2024-11-06 23:18:43 +0900

    404 style

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for doesn't exist.</p>
-        <router-link to="/">Go back to Home</router-link>
+    <div class="page-404">
+        <h1>Error 404</h1>
+        <img src="@/assets/images/sorry.png" alt="">
+        <span>お探しのページが見つかりませんでした</span>
+        <router-link to="/"><button class="button">ホームページへ戻る</button></router-link>
     </div>
 </template>
 
@@ -13,3 +14,28 @@ export default defineComponent({
     name: 'NotFound'
 });
 </script>
+
+<style lang="scss" scoped>
+.page-404 {
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
+    text-align: center;
+
+    h1 {
+        font-size: 50px;
+    }
+
+    span {
+        font-size: 32px;
+        color: #757575;
+        margin-bottom: 20px;
+    }
+
+    img {
+        width: 270px;
+        margin: 0 auto;
+    }
+
+}
+</style>
