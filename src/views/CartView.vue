0b675fc75c69080759ad9919da49d42b55434621
Rafis237026 <k237026@kccollege.ac.jp>
2024-11-11 12:13:40 +0900

    fix checkout

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,241 +1,195 @@
 <template>
-<h2 style="text-align: left;">カート</h2>
+  <h2 style="text-align: left">カート</h2>
 
-
-<div class="product-list">
-
+  <div class="product-list">
     <div class="product-cart">
-
-  <div class="product-info">
-      <div class="product-img">
-        <img :src="image" :alt="name" class="product-image" />
-      </div>
-      <div class="price-info">
-        <div class="price-title">
-          <h3  class="product-title">{{ name }}</h3>
-          <p class="product-title">{{ englishName }}</p>
-          <div class="price-size">
-          <div class="size-price">
-            <span class="font-weight">身長</span>
-            <span>150cm</span>
+      <div class="product-info">
+        <div class="product-img">
+          <img :src="image" :alt="name" class="product-image" />
+        </div>
+        <div class="price-info">
+          <div class="price-title">
+            <h3 class="product-title">{{ name }}</h3>
+            <p class="product-title">{{ englishName }}</p>
+            <div class="price-size">
+              <div class="size-price">
+                <span class="font-weight">身長</span>
+                <span>150cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">肩幅</span>
+                <span>40cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">ウェストサイズ</span>
+                <span>70cm</span>
+              </div>
+              <span class="final-price font-weight">30,000￥</span>
+            </div>
           </div>
-          <div class="size-price">
-            <span class="font-weight">肩幅</span>
-            <span>40cm</span>
+          <div class="option-price">
+            <div class="option-wrap">
+              <span class="font-weight">生地の種類</span>
+              <span
+                >コットン (綿) <span class="font-weight">+1000￥</span></span
+              >
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">生地の色</span>
+              <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">裏地の種類</span>
+              <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">ボタンの種類</span>
+              <span
+                >プラスチックボタン
+                <span class="font-weight">+1000￥</span></span
+              >
+            </div>
           </div>
-          <div class="size-price">
-            <span class="font-weight">ウェストサイズ</span>
-            <span>70cm</span>
+        </div>
+      </div>
+      <div class="product-final-price">
+        <div class="delete-box"><a class="product-delete">Delete</a></div>
+        <div class="final-price">
+          <span>会計</span>
+          <span>100,000￥</span>
+        </div>
+      </div>
+    </div>
+    <div class="product-cart">
+      <div class="product-info">
+        <div class="product-img">
+          <img :src="image" :alt="name" class="product-image" />
+        </div>
+        <div class="price-info">
+          <div class="price-title">
+            <h3 class="product-title">{{ name }}</h3>
+            <p class="product-title">{{ englishName }}</p>
+            <div class="price-size">
+              <div class="size-price">
+                <span class="font-weight">身長</span>
+                <span>150cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">肩幅</span>
+                <span>40cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">ウェストサイズ</span>
+                <span>70cm</span>
+              </div>
+              <span class="final-price font-weight">30,000￥</span>
+            </div>
           </div>
-          <span class="final-price font-weight">30,000￥</span>
-        </div>
-        </div>
-        <div class="option-price">
-
-          <div class="option-wrap">
-            <span class="font-weight">生地の種類</span>
-            <span>コットン (綿) <span class="font-weight">+1000￥</span></span>
+          <div class="option-price">
+            <div class="option-wrap">
+              <span class="font-weight">生地の種類</span>
+              <span
+                >コットン (綿) <span class="font-weight">+1000￥</span></span
+              >
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">生地の色</span>
+              <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">裏地の種類</span>
+              <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">ボタンの種類</span>
+              <span
+                >プラスチックボタン
+                <span class="font-weight">+1000￥</span></span
+              >
+            </div>
           </div>
-          <div class="option-wrap">
-            <span class="font-weight">生地の色</span>
-            <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
+        </div>
+      </div>
+      <div class="product-final-price">
+        <div class="delete-box"><a class="product-delete">Delete</a></div>
+        <div class="final-price">
+          <span>会計</span>
+          <span>100,000￥</span>
+        </div>
+      </div>
+    </div>
+    <div class="product-cart">
+      <div class="product-info">
+        <div class="product-img">
+          <img :src="image" :alt="name" class="product-image" />
+        </div>
+        <div class="price-info">
+          <div class="price-title">
+            <h3 class="product-title">{{ name }}</h3>
+            <p class="product-title">{{ englishName }}</p>
+            <div class="price-size">
+              <div class="size-price">
+                <span class="font-weight">身長</span>
+                <span>150cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">肩幅</span>
+                <span>40cm</span>
+              </div>
+              <div class="size-price">
+                <span class="font-weight">ウェストサイズ</span>
+                <span>70cm</span>
+              </div>
+              <span class="final-price font-weight">30,000￥</span>
+            </div>
           </div>
-          <div class="option-wrap">
-            <span class="font-weight">裏地の種類</span>
-            <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
+          <div class="option-price">
+            <div class="option-wrap">
+              <span class="font-weight">生地の種類</span>
+              <span
+                >コットン (綿) <span class="font-weight">+1000￥</span></span
+              >
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">生地の色</span>
+              <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">裏地の種類</span>
+              <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
+            </div>
+            <div class="option-wrap">
+              <span class="font-weight">ボタンの種類</span>
+              <span
+                >プラスチックボタン
+                <span class="font-weight">+1000￥</span></span
+              >
+            </div>
           </div>
-          <div class="option-wrap">
-            <span class="font-weight">ボタンの種類</span>
-            <span>プラスチックボタン <span class="font-weight">+1000￥</span></span>
-          </div>
-
         </div>
       </div>
-  </div>
-  <div class="product-final-price">
-    <div class="delete-box"><a class="product-delete">Delete</a></div>
-    <h2 class="final-price">会計  100,000￥</h2>
-  </div>
-
-  </div>    <div class="product-cart">
-
-<div class="product-info">
-    <div class="product-img">
-      <img :src="image" :alt="name" class="product-image" />
-    </div>
-    <div class="price-info">
-      <div class="price-title">
-        <h3  class="product-title">{{ name }}</h3>
-        <p class="product-title">{{ englishName }}</p>
-        <div class="price-size">
-        <div class="size-price">
-          <span class="font-weight">身長</span>
-          <span>150cm</span>
+      <div class="product-final-price">
+        <div class="delete-box"><a class="product-delete">Delete</a></div>
+        <div class="final-price">
+          <span>会計</span>
+          <span>100,000￥</span>
         </div>
-        <div class="size-price">
-          <span class="font-weight">肩幅</span>
-          <span>40cm</span>
-        </div>
-        <div class="size-price">
-          <span class="font-weight">ウェストサイズ</span>
-          <span>70cm</span>
-        </div>
-        <span class="final-price font-weight">30,000￥</span>
-      </div>
-      </div>
-      <div class="option-price">
-
-        <div class="option-wrap">
-          <span class="font-weight">生地の種類</span>
-          <span>コットン (綿) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">生地の色</span>
-          <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">裏地の種類</span>
-          <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">ボタンの種類</span>
-          <span>プラスチックボタン <span class="font-weight">+1000￥</span></span>
-        </div>
-
       </div>
     </div>
-</div>
-<div class="product-final-price">
-  <div class="delete-box"><a class="product-delete">Delete</a></div>
-  <h2 class="final-price">会計  100,000￥</h2>
-</div>
-
-</div>    <div class="product-cart">
-
-<div class="product-info">
-    <div class="product-img">
-      <img :src="image" :alt="name" class="product-image" />
-    </div>
-    <div class="price-info">
-      <div class="price-title">
-        <h3  class="product-title">{{ name }}</h3>
-        <p class="product-title">{{ englishName }}</p>
-        <div class="price-size">
-        <div class="size-price">
-          <span class="font-weight">身長</span>
-          <span>150cm</span>
-        </div>
-        <div class="size-price">
-          <span class="font-weight">肩幅</span>
-          <span>40cm</span>
-        </div>
-        <div class="size-price">
-          <span class="font-weight">ウェストサイズ</span>
-          <span>70cm</span>
-        </div>
-        <span class="final-price font-weight">30,000￥</span>
-      </div>
-      </div>
-      <div class="option-price">
-
-        <div class="option-wrap">
-          <span class="font-weight">生地の種類</span>
-          <span>コットン (綿) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">生地の色</span>
-          <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">裏地の種類</span>
-          <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">ボタンの種類</span>
-          <span>プラスチックボタン <span class="font-weight">+1000￥</span></span>
-        </div>
-
-      </div>
-    </div>
-</div>
-<div class="product-final-price">
-  <div class="delete-box"><a class="product-delete">Delete</a></div>
-  <h2 class="final-price">会計  100,000￥</h2>
-</div>
-
-</div>    <div class="product-cart">
-
-<div class="product-info">
-    <div class="product-img">
-      <img :src="image" :alt="name" class="product-image" />
-    </div>
-    <div class="price-info">
-      <div class="price-title">
-        <h3  class="product-title">{{ name }}</h3>
-        <p class="product-title">{{ englishName }}</p>
-        <div class="price-size">
-        <div class="size-price">
-          <span class="font-weight">身長</span>
-          <span>150cm</span>
-        </div>
-        <div class="size-price">
-          <span class="font-weight">肩幅</span>
-          <span>40cm</span>
-        </div>
-        <div class="size-price">
-          <span class="font-weight">ウェストサイズ</span>
-          <span>70cm</span>
-        </div>
-        <span class="final-price font-weight">30,000￥</span>
-      </div>
-      </div>
-      <div class="option-price">
-
-        <div class="option-wrap">
-          <span class="font-weight">生地の種類</span>
-          <span>コットン (綿) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">生地の色</span>
-          <span>赤 (あか) <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">裏地の種類</span>
-          <span>サテン裏地 <span class="font-weight">+1000￥</span></span>
-        </div>
-        <div class="option-wrap">
-          <span class="font-weight">ボタンの種類</span>
-          <span>プラスチックボタン <span class="font-weight">+1000￥</span></span>
-        </div>
-
-      </div>
-    </div>
-</div>
-<div class="product-final-price">
-  <div class="delete-box"><a class="product-delete">Delete</a></div>
-  <h2 class="final-price">会計  100,000￥</h2>
-</div>
-
-</div>
-</div>
-
-<div class="checkout-box">
-
-  <div class="list-pricebox">
-
-    <div class="list-price">
-      <span>会計</span>
-      <span>100,000￥</span>
-    </div>
-    <div class="list-btn">
-      <router-link to="/catalog"><span>戻る</span></router-link>
-      <button class="button" @click="goToAboutPage" >チェックアウト</button>
-    </div>
-
   </div>
 
-</div>
-
+  <div class="checkout-box">
+    <div class="list-pricebox">
+      <div class="list-price">
+        <span>会計</span>
+        <span>100,000￥</span>
+      </div>
+      <div class="list-btn">
+        <router-link to="/catalog"><span>戻る</span></router-link>
+        <button class="button" @click="goToAboutPage">チェックアウト</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -244,8 +198,8 @@ import { defineComponent, computed } from "vue";
 export default defineComponent({
   methods: {
     goToAboutPage() {
-      this.$router.push('../checkout')
-    }
+      this.$router.push("../checkout");
+    },
   },
   name: "ProductCard",
   props: {
@@ -276,7 +230,6 @@ export default defineComponent({
       () => `¥${props.price.toLocaleString("ja-JP")}〜`
     );
 
-
     return {
       formattedPrice,
     };
@@ -285,113 +238,107 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.font-weight{
+.font-weight {
   font-weight: 700;
   font-size: 16px;
 }
 
-h3{
+h3 {
   font-weight: 600;
   font-size: 24px;
   margin: 0;
 }
-.product-title{
+.product-title {
   margin: 0;
 }
 
-.product-cart{
+.product-cart {
   border: 1px solid #d9d9d9;
   border-radius: 8px;
   display: flex;
   justify-content: space-between;
   margin: 24px 0;
 
-
-  .product-info{
+  .product-info {
     display: flex;
     gap: 24px;
     margin: 24px 0 24px 24px;
 
-    .price-title{
+    .price-title {
       margin-bottom: 16px;
     }
 
-    .price-size{
+    .price-size {
       display: flex;
       gap: 12px;
 
-      .size-price{
+      .size-price {
         width: max-content;
         display: flex;
         gap: 5px;
         align-items: baseline;
       }
-
     }
-    .option-wrap{
+    .option-wrap {
       display: flex;
       justify-content: space-between;
     }
   }
-  .product-final-price{
+  .product-final-price {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
 
-    h2{
+    .final-price {
+      display: flex;
+      gap: 16px;
       margin: 10px 24px;
-      font-weight: 600;
-      font-size: 24px;
-      width: max-content;
+      span {
+        font-weight: 600;
+        font-size: 24px;
+      }
     }
 
-    .delete-box{
+    .delete-box {
       text-align: right;
       margin: 24px 24px 0 24px;
 
-      .product-delete{
-      width: min-content;
-      font-weight: 400;
-      color: #ec221f;
-      position: relative;
-      &::before{
-        content: url(../assets/icons/delete.svg);
-        display: block;
-        position: absolute;
-        right: 50px;
-      }
+      .product-delete {
+        width: min-content;
+        font-weight: 400;
+        color: #ec221f;
+        position: relative;
+        &::before {
+          content: url(../assets/icons/delete.svg);
+          display: block;
+          position: absolute;
+          right: 50px;
+        }
       }
     }
-
   }
-
-
 }
 
-.checkout-box{
-
+.checkout-box {
   width: 100%;
   display: flex;
   justify-content: right;
 
-
   .list-price,
-  .list-btn{
+  .list-btn {
     display: flex;
     gap: 16px;
     align-items: center;
     margin-bottom: 24px;
     justify-content: space-between;
 
-    span{
-    font-weight: 600;
-    font-size: 24px;
+    span {
+      font-weight: 600;
+      font-size: 24px;
     }
 
-
     a {
-
-      span{
+      span {
         font-weight: 400;
         font-size: 16px;
       }
@@ -407,30 +354,15 @@ h3{
         background: #f5f5f5;
         border-radius: 8px;
         padding: 10px;
-   }
+      }
+    }
   }
-
-
-  }
-
-
-
 }
 
-
-
-
-.product-image{
+.product-image {
   width: 100%;
 }
-.product-img{
+.product-img {
   width: 30%;
 }
-
-
-
-
-
-
-
 </style>
--- src/views/CheckOutView.vue
+++ src/views/CheckOutView.vue
@@ -21,17 +21,43 @@
         <span class="icon-pencil"></span>
 
       </div>
+      <div class="adress-card">
+        <h2>山田太郎</h2>
+        <p>240-0062, 神奈川県横浜市保土ケ谷区岡沢町, ４０１－６</p>
+        <div class="tel">
+          <span>電話番号: </span>
+          <span>080729709776</span>
+        </div>
+        <span class="icon-close"></span>
+        <span class="icon-pencil"></span>
+
+      </div>
+      
 
       <div class="add-card">
         <span class="icon-plus"></span>
         <p>新しいお届け先住所を追加する</p>
-
-
       </div>
 
     </div>
 
     <div class="order-wrap">
+      <h2>お支払方法</h2>
+      <div class="card-wrap">
+        <p>クレジットカード</p>
+        <div class="cardchoise-wrap">
+          <input class="radioBtn" type="radio" id="one" value="One" v-model="picked" />
+          <label for="Visa 末尾 9696">Visa 末尾 9696</label>
+        </div>
+        <div class="cardchoise-wrap">
+          <input class="radioBtn" type="radio" id="one" value="One" v-model="picked" />
+          <label for="Visa 末尾 9696">Visa 末尾 9696</label>
+        </div>
+        <a class="addcard">
+          クレジットカードまたはデビットカードを追加する
+        </a>
+      </div>
+
 
     </div>
   </div>
@@ -54,27 +80,36 @@
   text-align: left;
  }
 
-
-
- .adress-wrap{
+ .checkout-content{
   border: 1px solid #d9d9d9;
   border-radius: 8px;
   padding: 24px;
+ }
 
 
+ .adress-wrap{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+
+ 
 
 
   .adress-card{
     border: 1px solid #d9d9d9;
     border-radius: 8px;
+    position: relative;
     padding: 32px;
-    position: 100%;
+    box-sizing: border-box;
+    width: 100%;
 
 
 
     .icon-close{
       width: min-content;
       position: absolute;
+      top: 0;
+      right: 0;
       top: 10px;
       right: 10px;
       &::before{
@@ -99,26 +134,79 @@
 
 
   .add-card{
-
     position: relative;
+    border: 2px dashed #d9d9d9;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 32px;
+    box-sizing: border-box;
+
+
     .icon-plus{
       width: min-content;
       font-weight: 400;
-      color: #ec221f;
-      position: relative;
+      opacity: 50%;
+
       &::before{
         content: url(../assets/icons/plus.svg);
         display: block;
-        position: absolute;
-        right: 50px;
-        opacity: 50%;
       }
-      }
+    }
+    p{opacity: 50%;}
   }
 
 
  }
 
+ .order-wrap{
+
+
+  .cardchoise-wrap{
+    display: flex;
+    align-items: baseline;
+    gap: 50px;
+
+    .radioBtn{
+      position: relative;
+      display: block;
+      height: min-content;
+      margin-bottom: 24px;
+      &::before{
+        content: url(../assets/icons/visa.svg);
+        display: block;
+        position: absolute;
+        left: 20px;
+        bottom: -4px;
+        height: 21px;
+      }
+    }
+  }
+  .addcard{
+    padding-left: 30px;
+    position: relative;
+    &::before{
+        content: url(../assets/icons/plus.svg);
+        display: block;
+        position: absolute;
+        transform: scale(0.5); 
+        bottom: -10px;
+      }
+
+  }
+ }
+
   
+
+
+
+
+
+
+
+
 </style>
   
